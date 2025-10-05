2583f09f37c4c80d80f927591c7fb4907c8449ec
Saidu Conteh <csaidue@gmail.com>
2025-10-05 12:10:11 +0000

    Added a check for the production database url first

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,11 +1,18 @@
-
 import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma || new PrismaClient()
+const databaseUrl = process.env.PRODUCTION_DATABASE_URL || process.env.DATABASE_URL;
+
+const prisma = globalThis.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+})
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
