01e65c21cdf629e0cec00e2775080a3ee8034063
Saidu Conteh <csaidue@gmail.com>
2025-10-05 23:49:57 +0000

    Routes

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -11,7 +11,7 @@ export async function action({ request }: ActionFunctionArgs) {
     await logoutUser(refreshToken);
   }
 
-  return redirect("login", {
+  return redirect("/login", {
     headers: {
       "Set-Cookie": await destroySession(session),
     },
